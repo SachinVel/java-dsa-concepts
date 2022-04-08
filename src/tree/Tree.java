1137aabf8a841ff18b81e1a7c6afb38571726db0
sachin.v <sachin.v@zohocorp.com>
2022-04-08 11:14:51 +0530

    add some tree and string problems

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
-import trial.Try.Node;
 
 public class Tree {
 	
--- src/trial/Try.java
+++ src/trial/Try.java
@@ -4,33 +4,8 @@ import java.util.Scanner;
 
 public class Try {
 	
-	
-	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        for( int case=1 ; case<=t ; ++case ){
-            int n = sc.nextInt();
-            int k = sc.nextInt();
-            int arr[] = new int[n];
-            int res[] = new int[n];
-            int resInd;
-            for( int ind=0 ; ind<n ; ++ind ){
-
-                arr[ind]. sc.nextInt();
-                resInd = ind+k;
-                if( resInd>=n ){
-                    resInd %= n;
-                }
-                res[resInd] = arr[ind];
-            }
-            for( int ind=0 ; ind<n ; ++ind ){
-                System.out.print(res[ind]+" ");
-            }
-            System.out.println();
-        
-        }
+		
 
 		
 	}	

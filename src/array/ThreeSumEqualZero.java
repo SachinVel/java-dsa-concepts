c5ffad56dc01052d3e4d8e01e0e437ca22d1fa86
sachin <velsachin98@gmail.com>
2022-04-20 22:43:53 +0530

    add ThreeSumZero class

--- src/array/ThreeSumEqualZero.java
+++ src/array/ThreeSumEqualZero.java
@@ -1,10 +1,16 @@
 package array;
 
 import java.util.ArrayList;
+
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+
+/* 
+ *  https://leetcode.com/problems/3sum/
+ */
 
 public class ThreeSumEqualZero {
 	public static List<List<Integer>> threeSum(int nums[]){
@@ -13,51 +19,40 @@ public class ThreeSumEqualZero {
 			return result;
 		}
 		Arrays.parallelSort(nums);
-		HashMap<Integer,List<List<Integer>>> twoNumSums = new HashMap<Integer, List<List<Integer>>>();
 		
-		for( int ind1=0; ind1<nums.length-1 ; ++ind1 ) {
-			for( int ind2=ind1+1 ; ind2<nums.length ; ++ind2 ) {
-				List<Integer> pairs = new ArrayList<Integer>();
-				pairs.add(ind1);
-				pairs.add(ind2);
-				int pairSum = nums[ind1]+nums[ind2];
-				List<List<Integer>> pairList = twoNumSums.get(pairSum);
-				if( pairList==null ) {
-					pairList = new ArrayList<List<Integer>>();
-					pairList.add(pairs);
-					twoNumSums.put(pairSum,pairList);
-				}else {
-					pairList.add(pairs);
-				}
+		int desiredSum = 0;
+		for( int firstInd=0 ; firstInd<=nums.length-3 ; ++firstInd ) {
+			if( (firstInd-1)>=0 && nums[firstInd-1]==nums[firstInd] ) {
+				continue;
 			}
-		}
-		HashSet<Integer> hashValues = new HashSet<Integer>();
-		
-		for( int ind=0 ; ind<nums.length ; ++ind ) {
-			int compNum = -nums[ind];
-			if( twoNumSums.get(compNum)!=null ) {
-				for( List<Integer> pairs : twoNumSums.get(compNum) ) {
-					if( ind!=pairs.get(0) && ind!=pairs.get(1) && ind>pairs.get(0) && ind>pairs.get(1) ) {
-						int hashValue = nums[ind]*1+nums[pairs.get(0)]*2+nums[pairs.get(1)]*3;
-						if( !hashValues.contains(hashValue)  ) {
-							List<Integer> triplets = new ArrayList<Integer>();
-							triplets.add(nums[ind]);
-							triplets.add(nums[pairs.get(0)]);
-							triplets.add(nums[pairs.get(1)]);
-							result.add(triplets);
-							hashValues.add(hashValue);
-						}
-						
+			int secondInd = firstInd+1;
+			int thirdInd = nums.length-1;
+			while( secondInd<thirdInd ) {
+				int curTripletSum = nums[firstInd]+nums[secondInd]+nums[thirdInd];
+				if( curTripletSum==desiredSum ) {
+					List<Integer> triplet = new ArrayList<Integer>();
+					triplet.add(nums[firstInd]);
+					triplet.add(nums[secondInd]);
+					triplet.add(nums[thirdInd]);
+					result.add(triplet);
+					++secondInd;
+					while( nums[secondInd]==nums[secondInd-1] && secondInd<thirdInd) {
+						++secondInd;
 					}
-					
+				}else if( curTripletSum<desiredSum ) {
+					++secondInd; 
+				}else {
+					--thirdInd;
 				}
 			}
+			
 		}
 		
 		return result;
 	}
 	public static void main(String[] args) {
 		int nums[] = {-1,0,1,2,-1,-4};
+		
 		List<List<Integer>> result = threeSum(nums);
 		System.out.println(result);
 		
